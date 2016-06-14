196a3d1c1def8e78ff244c9b3cce4636ae8553e4
DanteLore <dan@logicalgenetics.com>
2016-06-14 13:44:41 +0100

    Changed to the newest version of the SOAP API

--- collectors/rail-collector.py
+++ collectors/rail-collector.py
@@ -1,4 +1,5 @@
 import argparse
+from time import sleep
 
 import requests
 import xmltodict
@@ -11,17 +12,18 @@ import xmltodict
 from utils.database import insert_into_db
 
 xml_payload = """<?xml version="1.0"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://thalesgroup.com/RTTI/2014-02-20/ldb/" xmlns:ns2="http://thalesgroup.com/RTTI/2010-11-01/ldb/commontypes">
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://thalesgroup.com/RTTI/2016-02-16/ldb/" xmlns:ns2="http://thalesgroup.com/RTTI/2013-11-28/Token/types">
   <SOAP-ENV:Header>
     <ns2:AccessToken>
       <ns2:TokenValue>{KEY}</ns2:TokenValue>
     </ns2:AccessToken>
   </SOAP-ENV:Header>
   <SOAP-ENV:Body>
-    <ns1:GetDepartureBoardRequest>
+    <ns1:GetDepBoardWithDetailsRequest>
       <ns1:numRows>100</ns1:numRows>
       <ns1:crs>{CRS}</ns1:crs>
-    </ns1:GetDepartureBoardRequest>
+      <ns1:timeWindow>100</ns1:timeWindow>
+    </ns1:GetDepBoardWithDetailsRequest>
   </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>
 """
@@ -33,26 +35,28 @@ def fetch_trains(url, key, crs):
     response = requests.post(url, data=payload, headers=headers)
 
     data = xmltodict.parse(response.content)
-    services = data["soap:Envelope"]["soap:Body"]["GetDepartureBoardResponse"]["GetStationBoardResult"]["lt2:trainServices"]["lt2:service"]
+    services = data["soap:Envelope"]["soap:Body"]["GetDepBoardWithDetailsResponse"]["GetStationBoardResult"]["lt5:trainServices"]["lt5:service"]
 
     for service in services:
         yield {
             "crs": crs,
-            "origin": service["lt2:origin"]["lt2:location"]["lt2:locationName"],
-            "destination": service["lt2:destination"]["lt2:location"]["lt2:locationName"],
-            "std": service.get("lt2:std"),
-            "etd": service.get("lt2:etd"),
-            "platform": service.get("lt2:platform", "-"),
+            "origin": service["lt5:origin"]["lt4:location"]["lt4:locationName"],
+            "destination": service["lt5:destination"]["lt4:location"]["lt4:locationName"],
+            "std": service.get("lt4:std"),
+            "etd": service.get("lt4:etd"),
+            "platform": service.get("lt4:platform", "-"),
         }
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='National Rail Data Collector')
     parser.add_argument('--key', help='API Key', required=True)
-    parser.add_argument('--url', help='API URL', default="http://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb6.asmx")
+    parser.add_argument('--url', help='API URL', default="http://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb9.asmx")
     parser.add_argument('--crs', help='CRS Station Code (default is Thatcham)', default="THA")
     parser.add_argument('--db', help='SQLite DB Name', default="../data/trains.db")
     args = parser.parse_args()
 
-    departures = fetch_trains(args.url, args.key, args.crs)
-    insert_into_db(args.db, "departures", departures)
+    while True:
+        departures = fetch_trains(args.url, args.key, args.crs)
+        insert_into_db(args.db, "departures", departures)
+        sleep(10)
